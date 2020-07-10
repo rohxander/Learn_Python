32fcc3fdeecfa9117f1cb09be33475ad0ab35fc4
rohxander <32710857+rohxander@users.noreply.github.com>
2020-07-10 12:36:26 +0530

    Add files via upload

---
+++ ex13.py
@@ -0,0 +1,11 @@
+from sys import argv
+
+script, one, two, three = argv
+
+print "The script is called:", script
+print "Your first variable is:", one
+print "Your second variable is:", two
+print "Your third variable is:", three
+
+
+
