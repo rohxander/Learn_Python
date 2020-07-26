1893fdc91092c8011d664ea62286f266d0cbc666
Rohit Prakash <rohitmanjappallil@gmail.com>
2020-07-26 12:10:40 +0530

    seven exercises push!!

---
+++ ex6.py
@@ -0,0 +1,18 @@
+print("Mary had a little lamb.")
+print("Its fleece was white as {}.".format('snow'))
+print("And everywhere that Mary went.")
+print("." * 10) 
+
+end1 = "C"
+end2 = "h"
+end3 = "a"
+end4 = "r"
+end5 = "l"
+end6 = "i"
+end7 = "e"
+end8 = "C"
+end9 = "o"
+end10 = "p"
+
+print(end1 + end2 + end3 + end4 + end5 + end6 + end7, end=' ')
+print( end8 + end9 + end10 )
