a9e2de05f0b467c3437d45d06d5bce52f6774ef3
rohxander <rohitmanjappallil@gmail.com>
2020-07-28 11:19:11 +0530

    till exercise 14

--- ex1.py
+++ ex1.py
@@ -1,4 +1,3 @@
-print("Hello World!")
-print("Hello Again")
-print("will this print numbers like 12 3eresdgsdfadsg \"fdgdfgdgfdfg\"")
-print("Testing Commit through Sublime Text Editor")
+Hello
+this
+world
